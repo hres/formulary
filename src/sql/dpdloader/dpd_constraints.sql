11abd2e6c83f5cf4a7bf081e0255fcae1f23b788
Nick Matantsev <nick.matantsev@gmail.com>
2018-02-06 11:19:42 -0500

    Add test DPD extract for SQL processing

---
+++ src/sql/dpdloader/dpd_constraints.sql
@@ -0,0 +1,33 @@
+CREATE INDEX companies_drug_code ON dpd.companies USING btree (extract, drug_code);
+CREATE INDEX packaging_drug_code ON dpd.packaging USING btree (extract, drug_code);
+CREATE INDEX pharmaceutical_form_drug_code ON dpd.pharmaceutical_form USING btree (extract, drug_code);
+CREATE INDEX pharmaceutical_std_drug_code ON dpd.pharmaceutical_std USING btree (extract, drug_code);
+CREATE INDEX schedule_drug_code ON dpd.schedule USING btree (extract, drug_code);
+CREATE INDEX status_drug_code ON dpd.status USING btree (extract, drug_code);
+CREATE INDEX active_ingredient_drug_code ON dpd.active_ingredient USING btree (extract, drug_code);
+CREATE INDEX therapeutic_class_drug_code ON dpd.therapeutic_class USING btree (extract, drug_code);
+CREATE INDEX route_drug_code ON dpd.route USING btree (extract, drug_code);
+CREATE INDEX vet_drug_code ON dpd.vet_species USING btree (extract, drug_code);
+
+ALTER TABLE dpd.drug_product ADD CONSTRAINT drug_product_drug_code PRIMARY KEY (extract, drug_code);
+
+ALTER TABLE dpd.packaging ADD CONSTRAINT packaging_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.pharmaceutical_form ADD CONSTRAINT pharmaceutical_form_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.pharmaceutical_std ADD CONSTRAINT pharmaceutical_std_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.schedule ADD CONSTRAINT schedule_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.status ADD CONSTRAINT status_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.therapeutic_class ADD CONSTRAINT ther_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.vet_species ADD CONSTRAINT vet_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.active_ingredient ADD CONSTRAINT active_ingredient_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.companies ADD CONSTRAINT companies_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
+ALTER TABLE dpd.route ADD CONSTRAINT route_drug_code_fkey FOREIGN KEY (extract, drug_code) REFERENCES dpd.drug_product(extract, drug_code) NOT DEFERRABLE;
+
---
+++ src/sql/dpdloader/dpd_structure.sql
@@ -0,0 +1,132 @@
+DROP SCHEMA IF EXISTS dpd CASCADE;
+CREATE SCHEMA dpd;
+
+CREATE TABLE dpd.drug_product (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    product_categorization text,
+    class text,
+    drug_identification_number text NOT NULL,
+    brand_name text NOT NULL,
+    descriptor text,
+    pediatric_flag text,
+    accession_number text,
+    number_of_ais text,
+    last_update_date date,
+    ai_group_no text,
+    class_f text,
+    brand_name_f text,
+    descriptor_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.packaging (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    upc text,
+    package_size_unit text,
+    package_type text,
+    package_size text,
+    product_information text,
+    package_size_unit_f text,
+    package_type_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.pharmaceutical_form (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    pharm_form_code integer,
+    pharmaceutical_form text,
+    pharmaceutical_form_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.pharmaceutical_std (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    pharmaceutical_std text
+) WITH (oids = false);
+
+CREATE TABLE dpd.schedule (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    schedule text,
+    schedule_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.status (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    current_status_flag text,
+    status text,
+    history_date date,
+    status_f text,
+    lot_number text,
+    expiration_date date
+) WITH (oids = false);
+
+CREATE TABLE dpd.therapeutic_class (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    tc_atc_number text,
+    tc_atc text,
+    tc_ahfs_number text,
+    tc_ahfs text,
+    tc_atc_f text,
+    tc_ahfs_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.vet_species (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    vet_species text,
+    vet_sub_species text,
+    vet_species_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.active_ingredient (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    active_ingredient_code integer NOT NULL,
+    ingredient text NOT NULL,
+    ingredient_supplied_ind text,
+    strength text NOT NULL,
+    strength_unit text NOT NULL,
+    strength_type text,
+    dosage_value text,
+    base text,
+    dosage_unit text,
+    notes text,
+    ingredient_f text,
+    strength_unit_f text,
+    strength_type_f text,
+    dosage_unit_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.companies (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    mfr_code text,
+    company_code integer NOT NULL,
+    company_name text NOT NULL,
+    company_type text,
+    address_mailing_flag text,
+    address_billing_flag text,
+    address_notification_flag text,
+    address_other text,
+    suite_number text,
+    street_name text,
+    city_name text,
+    province text,
+    country text,
+    postal_code text,
+    post_office_box text,
+    province_f text,
+    country_f text
+) WITH (oids = false);
+
+CREATE TABLE dpd.route (
+    extract text DEFAULT 'active',
+    drug_code integer NOT NULL,
+    route_of_administration_code text,
+    route_of_administration text,
+    route_of_administration_f text
+) WITH (oids = false);
