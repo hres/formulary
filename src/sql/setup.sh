302d215ecc136ee77125a1b881ac0a25c35b96bc
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2018-10-01 11:40:26 -0400

    update date in config files for October generation

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Must set environment variables PGHOST, PGUSER and PGPASSWORD. PGDATABASE must be unset
 
-ccdd_qa_release_date="20180802"
-ccdd_current_release_date="20180810"
+ccdd_qa_release_date="20180905"
+ccdd_current_release_date="20180911"
 ccdd_current_date=$(date +'%Y%m%d')
 
 
