f6bde3aed52dfdb03a9f89c06c9595b09333d447
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2019-01-07 12:33:35 -0500

    code updates

--- src/sql/test/upates_for_Release.R
+++ src/sql/test/upates_for_Release.R
@@ -29,7 +29,8 @@ code_to_add<-left_join(code_to_add,dpd_drug[,c('drug_code','drug_identification_
 code_to_add<-code_to_add%>%slice(1:35)
 
 
-whitelist<-read.csv('./src/sql/test/ccdd-mp-whitelist-draft.csv',stringsAsFactors = F)
+whitelist<-read.csv('./src/sql/test/ccdd-mp-whitelist-draft.csv',stringsAsFactors = F,
+                    colClasses = c('mp_code'='character'))
 pseudodin<-read.csv('./src/sql/test/ccdd-pseudodin-map-draft.csv',stringsAsFactors = F)
 
 pseudodin$pseudodin<-as.character(pseudodin$pseudodin)
@@ -38,10 +39,10 @@ code_to_add$drug_code<-coalesce(code_to_add$drug_code.x,code_to_add$drug_code.y)
 
 
 #include DIN and mp concept name to whitelist:
-whitelist<-left_join(whitelist,dpd_drug[,c('drug_code','drug_identification_number')],copy=T)
-whitelist<-left_join(whitelist,tbl(ccdd, in_schema("ccdd","mp_release_candidate"))%>%select(mp_code,mp_formal_name),
-                     by=c('drug_identification_number'='mp_code'),copy=T)
-whitelist<-rename(whitelist,mp_code=drug_identification_number)
+# whitelist<-left_join(whitelist,dpd_drug[,c('drug_code','drug_identification_number')],copy=T)
+# whitelist<-left_join(whitelist,tbl(ccdd, in_schema("ccdd","mp_release_candidate"))%>%select(mp_code,mp_formal_name),
+#                      by=c('drug_identification_number'='mp_code'),copy=T)
+# whitelist<-rename(whitelist,mp_code=drug_identification_number)
 
 whitelist<-bind_rows(whitelist,code_to_add[,c(1,2,5)])%>%distinct()
 
@@ -50,7 +51,7 @@ write.csv(whitelist,'./src/sql/test/ccdd-mp-whitelist-draft.csv',row.names = F)
 
 # first run write-new-concetps.sql script to update tm_definition_draft
 #update TM_filter_master.csv
-tm_filter<-read.csv('./src/sql/test/TM_filter_20181206.csv',stringsAsFactors = F)
+tm_filter<-read.csv('./src/sql/test/TM_filter_20190107.csv',stringsAsFactors = F)
 tm_definition<-read.csv('./src/sql/test/ccdd-tm-definitions-draft.csv',stringsAsFactors = F)
 tm_filter_master<-tm_definition%>%filter(formal_name%in% tm_filter$tm_formal_name)
 
