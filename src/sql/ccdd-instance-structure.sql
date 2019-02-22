86645bbacad911599344f8ef6910634deca635e2
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2019-02-22 11:42:49 -0500

    Fix NA values in combination table

--- src/sql/ccdd-instance-structure.sql
+++ src/sql/ccdd-instance-structure.sql
@@ -1440,10 +1440,8 @@ SELECT
    cp.drug_code AS dpd_drug_code,
    cp.mp_formal_name,
    cp.ntp_formal_name,
-    (case 
-         when  cp.ntp_type='NA' then null
-         else  cp.ntp_type
-    END)
+   cp.ntp_type
+    
 FROM
    ccdd.combination_products_csv AS cp
 WHERE
