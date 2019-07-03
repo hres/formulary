a9d4b3b323f923430971e7efe3405ea673d27cee
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2019-07-03 11:00:25 -0400

    Add comments to script

--- src/sql/test/upates_for_Release.R
+++ src/sql/test/upates_for_Release.R
@@ -8,7 +8,7 @@ library(tidyr)
 
 ccdd <- dbPool(drv      = RPostgreSQL::PostgreSQL(),
                    host     = "rest.hc.local",
-                   dbname   = "ccdd_2019_06_03_110903",
+                   dbname   = "ccdd_2019_07_02_100803",
                    user     = "nzhu",
                    password = "nzhu_rest" )
 
@@ -17,10 +17,14 @@ dpd_drug<-tbl(ccdd, in_schema("dpd","drug_product"))%>%as.data.frame()
 changes_mp<-tbl(ccdd, in_schema("public","qa_release_changes_mp_release_candidate"))%>%as.data.frame()
 
 #changes_mp<-read.xlsx('TM NTP and MP Concepts to be added back for September 2018 CCDD Release.xlsx',3,stringsAsFactors=F,header=T)
-julie_whitelist<-read.csv('./src/sql/test/whitelist_20190605.csv',stringsAsFactors = F)
 code_to_add<-changes_mp[changes_mp$changes=='DELETED',1:2]
 code_to_add<-left_join(code_to_add,dpd_drug[,c('drug_code','drug_identification_number')],by=c('mp_code'='drug_identification_number'))
-code_to_add<-code_to_add%>%filter(drug_code %in% julie_whitelist$drug_code)
+
+
+#confirm with QA whitelist
+qa_whitelist_filepath<-'./src/sql/test/whitelist_20190605.csv'
+qa_whitelist<-read.csv(qa_whitelist_filepath,stringsAsFactors = F)
+code_to_add<-code_to_add%>%filter(drug_code %in% qa_whitelist$drug_code)
 
 
 whitelist<-read.csv('./src/sql/test/ccdd-mp-whitelist-draft.csv',stringsAsFactors = F,
@@ -45,31 +49,35 @@ write.csv(whitelist,'./src/sql/test/ccdd-mp-whitelist-draft.csv',row.names = F)
 
 # first run write-new-concetps.sql script to update tm_definition_draft
 #update TM_filter_master.csv
+
+#load tm filter from QA file, change filepath if necessary
 tm_filter<-read.csv('./src/sql/test/tm_filter_20190605.csv',stringsAsFactors = F)
+
+#load tm_definition after running write-new-concepts.sh 
 tm_definition<-read.csv('./src/sql/test/ccdd-tm-definitions-draft.csv',stringsAsFactors = F)
 tm_filter_master<-tm_definition%>%filter(formal_name%in% tm_filter$tm_formal_name)
 
-#save the TM_filter_master.csv to folder-reorg:
+#save the TM_filter_master.csv and move to folder-reorg branch:
 write.csv(tm_filter_master,'./src/sql/test/TM_filter_master.csv',row.names = F)
 
 
-#update black list:
+
 
 
 #update brand overide list :
-brand_overide<-read.csv('./src/sql/test/ccdd-mp-brand-override-draft.csv',stringsAsFactors = F)
-
-# list of products with duplicated MP name, find drug code and add dpd descriptors to brand-override 
-duplicate_mp<-read.xlsx('./src/sql/test/Changes for CCDD October 2018 Release Candidate.xlsx',2,stringsAsFactors=F)
-
-duplicate_mp<-duplicate_mp%>%
-              filter(Action=='Add DPD Descriptor')%>%
-              left_join(dpd_drug[,c('drug_code','drug_identification_number','brand_name')],
-                      by=c('mp_code'='drug_identification_number'))%>%
-              select(drug_code,brand_name,DPD.Descriptor)%>%
-              unite('brand',c('brand_name','DPD.Descriptor'),sep=' ')
-
-brand_overide<-bind_rows(brand_overide,duplicate_mp)
-brand_overide$drug_code<-as.character(brand_overide$drug_code)
-
-write.csv(brand_overide,'./src/sql/test/ccdd-mp-brand-override-draft.csv',row.names = F)
+# brand_overide<-read.csv('./src/sql/test/ccdd-mp-brand-override-draft.csv',stringsAsFactors = F)
+# 
+# # list of products with duplicated MP name, find drug code and add dpd descriptors to brand-override 
+# duplicate_mp<-read.xlsx('./src/sql/test/Changes for CCDD October 2018 Release Candidate.xlsx',2,stringsAsFactors=F)
+# 
+# duplicate_mp<-duplicate_mp%>%
+#               filter(Action=='Add DPD Descriptor')%>%
+#               left_join(dpd_drug[,c('drug_code','drug_identification_number','brand_name')],
+#                       by=c('mp_code'='drug_identification_number'))%>%
+#               select(drug_code,brand_name,DPD.Descriptor)%>%
+#               unite('brand',c('brand_name','DPD.Descriptor'),sep=' ')
+# 
+# brand_overide<-bind_rows(brand_overide,duplicate_mp)
+# brand_overide$drug_code<-as.character(brand_overide$drug_code)
+# 
+# write.csv(brand_overide,'./src/sql/test/ccdd-mp-brand-override-draft.csv',row.names = F)
