ae9848b05d892b07f772fb772ea915fcb8563eb0
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2019-07-04 14:22:29 -0400

    updates

--- src/sql/test/upates_for_Release.R
+++ src/sql/test/upates_for_Release.R
@@ -22,19 +22,27 @@ code_to_add<-left_join(code_to_add,dpd_drug[,c('drug_code','drug_identification_
 
 
 #confirm with QA whitelist
-qa_whitelist_filepath<-'./src/sql/test/whitelist_20190605.csv'
-qa_whitelist<-read.csv(qa_whitelist_filepath,stringsAsFactors = F)
+qa_whitelist_filepath<-'./src/sql/test/whitelist_20190704.xlsx'
+qa_whitelist<-read.xlsx2(qa_whitelist_filepath,1,stringsAsFactors = F)
 code_to_add<-code_to_add%>%filter(drug_code %in% qa_whitelist$drug_code)
 
 
 whitelist<-read.csv('./src/sql/test/ccdd-mp-whitelist-draft.csv',stringsAsFactors = F,
                     colClasses = c('mp_code'='character'))
+
+# if there is a product with pseudo din on the list:
+if(sum(is.na(code_to_add$drug_code))>0){
 pseudodin<-read.csv('./src/sql/test/ccdd-pseudodin-map-draft.csv',stringsAsFactors = F)
 
 pseudodin$pseudodin<-as.character(pseudodin$pseudodin)
 code_to_add<-left_join(code_to_add,pseudodin[,c('pseudodin','drug_code')],by=c('mp_code'='pseudodin'))
 code_to_add$drug_code<-coalesce(code_to_add$drug_code.x,code_to_add$drug_code.y)
 
+code_to_add<-code_to_add[,c(1,2,5)]
+}else{
+  
+code_to_add<-code_to_add
+}
 
 #include DIN and mp concept name to whitelist:
 # whitelist<-left_join(whitelist,dpd_drug[,c('drug_code','drug_identification_number')],copy=T)
@@ -42,7 +50,7 @@ code_to_add$drug_code<-coalesce(code_to_add$drug_code.x,code_to_add$drug_code.y)
 #                      by=c('drug_identification_number'='mp_code'),copy=T)
 # whitelist<-rename(whitelist,mp_code=drug_identification_number)
 
-whitelist<-bind_rows(whitelist,code_to_add[,c(1,2,5)])%>%distinct()
+whitelist<-bind_rows(whitelist,code_to_add)%>%distinct()
 
 write.csv(whitelist,'./src/sql/test/ccdd-mp-whitelist-draft.csv',row.names = F)
 
@@ -51,7 +59,7 @@ write.csv(whitelist,'./src/sql/test/ccdd-mp-whitelist-draft.csv',row.names = F)
 #update TM_filter_master.csv
 
 #load tm filter from QA file, change filepath if necessary
-tm_filter<-read.csv('./src/sql/test/tm_filter_20190605.csv',stringsAsFactors = F)
+tm_filter<-read.csv('./src/sql/test/tm_filter_20190704.csv',stringsAsFactors = F)
 
 #load tm_definition after running write-new-concepts.sh 
 tm_definition<-read.csv('./src/sql/test/ccdd-tm-definitions-draft.csv',stringsAsFactors = F)
