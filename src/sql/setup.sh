7dc29f6638c4eedc34a173fc57b33998a8851200
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2019-11-25 10:58:07 -0500

    update dates

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Must set environment variables PGHOST, PGUSER and PGPASSWORD. PGDATABASE must be unset
 
-ccdd_qa_release_date="20191001"
-ccdd_current_release_date="20191002"
+ccdd_qa_release_date="20191101"
+ccdd_current_release_date="20191105"
 ccdd_current_date=$(date +'%Y%m%d')
 
 
