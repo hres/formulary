5f92952c9fd3cb29a4818e618fa4954580bfdd50
nancyzhu24 <nancy.zhu.mcgill@Gmail.com>
2020-02-24 13:52:41 -0500

    update comparison data

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Must set environment variables PGHOST, PGUSER and PGPASSWORD. PGDATABASE must be unset
 
-ccdd_qa_release_date="20200102"
-ccdd_current_release_date="20200108"
+ccdd_qa_release_date="20200203"
+ccdd_current_release_date="20200205"
 ccdd_current_date=$(date +'%Y%m%d')
 
 
