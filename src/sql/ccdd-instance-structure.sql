015cc262a51da841f39d2981e10fa427888bf29a
coreyyanofsky <corey.yanofsky@gmail.com>
2020-12-01 15:16:26 -0500

    add AUTHORIZED BY INTERIM ORDER case to status-handling code

--- src/sql/ccdd-instance-structure.sql
+++ src/sql/ccdd-instance-structure.sql
@@ -1512,6 +1512,7 @@ select
 			)
 		WHEN ds.current_status = 'MARKETED' THEN ds.first_market_date
 		WHEN ds.current_status = 'APPROVED' AND ds.first_market_date IS NOT NULL THEN ds.first_market_date
+		WHEN ds.current_status = 'AUTHORIZED BY INTERIM ORDER' AND ds.first_market_date IS NOT NULL THEN ds.first_market_date
 		WHEN ds.current_status = 'CANCELLED POST MARKET' AND ds.current_status_date < (SELECT ccdd_date FROM ccdd_config LIMIT 1) AND ds.current_expiration_date::date > (SELECT dpd_extract_date FROM ccdd_config LIMIT 1) THEN ds.first_market_date
 		ELSE ds.current_status_date
 	END) as mp_status_effective_date,
@@ -1521,11 +1522,9 @@ select
 				IN
 			(SELECT * FROM ccdd.mp_deprecations)
 		) THEN 'Deprec'
-		WHEN ds.current_status = 'MARKETED' OR (
-			ds.current_status = 'APPROVED'
-			AND
-			ds.first_market_date IS NOT NULL
-		) THEN 'Active'
+		WHEN ds.current_status = 'MARKETED' OR (ds.first_market_date IS NOT NULL AND (
+			  ds.current_status = 'APPROVED' OR ds.current_status = 'AUTHORIZED BY INTERIM ORDER'
+		)) THEN 'Active'
 		WHEN ds.current_status = 'CANCELLED POST MARKET' AND ds.current_expiration_date::date > (SELECT dpd_extract_date FROM ccdd_config LIMIT 1)  THEN 'Active'
 		ELSE 'Inactive'
 	END) as mp_status,
