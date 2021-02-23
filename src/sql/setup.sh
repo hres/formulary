c897486d3e3f79a2137e2df6bf95d0186c83f1b3
coreyyanofsky <corey.yanofsky@gmail.com>
2021-02-23 11:09:13 -0500

    update paths and dates

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 # Must set environment variables PGHOST, PGUSER and PGPASSWORD. PGDATABASE must be unset
-ccdd_qa_release_date="20210104"
-ccdd_current_release_date="20210105"
-db_previous_month="ccdd_2021_01_05_142530"
+ccdd_qa_release_date="20210201"
+ccdd_current_release_date="20210201"
+db_previous_month="ccdd_2021_02_01_170236"
 ccdd_current_date=$(date +'%Y%m%d')
 baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist/$ccdd_current_date"
-QA_changeDir="$distDir/${ccdd_current_date}_from_${ccdd_qa_release_date}"
+QA_changeDir="$distDir/QA_${ccdd_current_date}_from_${ccdd_qa_release_date}"
 release_changeDir="$distDir/${ccdd_current_date}_from_${ccdd_current_release_date}"
 
 
@@ -26,10 +26,11 @@ PGDATABASE=$PGUSER
 createdb "$PGDATABASE"
 
 # DPD extract load
-pgloader "$baseDir/dpdloader/dpdload.pgload"
+rm -rf /tmp/pgloader/* # make sure pgloader's assumptions about its temp directory are satisfied
+pgloader "$baseDir/dpdloader/dpdload.pgload" # has to be first because of a BEFORE LOAD EXECUTE
 pgloader "$baseDir/dpdloader/dpdload_ia.pgload"
 pgloader "$baseDir/dpdloader/dpdload_dr.pgload"
-pgloader "$baseDir/dpdloader/dpdload_ap.pgload"
+pgloader "$baseDir/dpdloader/dpdload_ap.pgload" # has to be last because of an AFTER LOAD EXECUTE
 
 
 # global config for CCDD generation process
@@ -55,14 +56,13 @@ pgloader "$baseDir/ccdd-current-release.pgload"
 # load the data from views into main schema
 psql -v ON_ERROR_STOP=1 < "$baseDir/ccdd-run-views.sql"
 
-
-# create output folder, then export CCDD concepts as CSV files to output
+# create output folders
 mkdir -p "$distDir"
 mkdir -p "$QA_changeDir"
 mkdir -p "$release_changeDir"
-mkdir -p "$distDir/DPD_diff"
 
 
+# export CCDD concepts as CSV files to output
 # QA release and candidate CSVs
 psql -c "copy ((select mp_code, mp_formal_name, COALESCE(mp_en_description, 'NA') as mp_en_description, COALESCE(mp_fr_description, 'NA')
         as mp_fr_description, mp_status, mp_status_effective_time, mp_type, \"Health_Canada_identifier\", \"Health_Canada_product_name\" FROM ccdd_mp_table)
@@ -167,6 +167,6 @@ psql -d ccdd -c "ALTER DATABASE $PGUSER RENAME TO $NEW_DB_NAME;"
 export PGDATABASE=$NEW_DB_NAME
 
 echo
-echo Generated "$PGDATABASE" and output in "$distDir"
+echo Generated  "$PGDATABASE" and output in "$distDir"
 
 
