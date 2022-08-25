0a84cc431f76fae9bda7723d18e170b58919f8f8
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2022-08-25 15:06:12 -0400

    Revert "update paths and dates"
    This reverts commit 44e977e6e94cf53c6fa37ea55e4cc3d6f11f6829.

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Must set environment variables PGHOST, PGUSER and PGPASSWORD. PGDATABASE must be unset
-ccdd_qa_release_date="20220802"
-ccdd_current_release_date="20220803"
-db_previous_month="ccdd_2022_08_03_152825"
+ccdd_qa_release_date="20220704"
+ccdd_current_release_date="20220704"
+db_previous_month="ccdd_2022_07_04_154323"
 ccdd_current_date=$(date +'%Y%m%d')
 baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist/$ccdd_current_date"
