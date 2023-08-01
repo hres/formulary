43cd4ec3d68555e990db74cb12497548646209e4
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2023-08-01 10:09:16 -0400

    updates for August 2023 QA generation

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -7,9 +7,9 @@
 # ARGS (optional)   : qa
 ###############################################################################
 
-ccdd_qa_release_date="20230601"
-ccdd_current_release_date="20230601"
-db_previous_month="ccdd_2023_06_01_145240"
+ccdd_qa_release_date="20230704"
+ccdd_current_release_date="20230704"
+db_previous_month="ccdd_2023_07_04_151043"
 ccdd_current_date=$(date +'%Y%m%d')
 baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist/$ccdd_current_date"
