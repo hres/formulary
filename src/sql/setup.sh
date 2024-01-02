fee1e1d15eaef74abbd10ab7fed7ebfa93199945
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2024-01-02 11:27:16 -0500

    updates for January 2024 QA generation

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -7,9 +7,9 @@
 # ARGS (optional)   : qa
 ###############################################################################
 
-ccdd_qa_release_date="20231101"
-ccdd_current_release_date="20231101"
-db_previous_month="ccdd_2023_11_01_150934"
+ccdd_qa_release_date="20231204"
+ccdd_current_release_date="20231205"
+db_previous_month="ccdd_2023_12_05_105406"
 ccdd_current_date=$(date +'%Y%m%d')
 baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist/$ccdd_current_date"
