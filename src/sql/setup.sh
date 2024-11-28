3b07b5e1350745b8f9f03eb25a164388704021d2
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2024-11-28 10:32:07 -0500

    update paths and dates

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Must set environment variables PGHOST, PGUSER and PGPASSWORD. PGDATABASE must be unset
-ccdd_qa_release_date="20241001"
-ccdd_current_release_date="20241009"
-db_previous_month="ccdd_2024_10_09_112516"
+ccdd_qa_release_date="20241101"
+ccdd_current_release_date="20241101"
+db_previous_month="ccdd_2024_11_04_150943"
 ccdd_current_date=$(date +'%Y%m%d')
 baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist/$ccdd_current_date"
