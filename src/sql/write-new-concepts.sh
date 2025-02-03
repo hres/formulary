ee27957adb14f7e61fd03c7eb20e71bcfd58eb46
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2025-02-03 09:35:45 -0500

    update write-new-concepts.sh with QA ccdd database name

--- src/sql/write-new-concepts.sh
+++ src/sql/write-new-concepts.sh
@@ -22,7 +22,7 @@ baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist"
 testDir="$baseDir/../sql/test"
 
-dbName="ccdd_2025_01_02_103000"
+dbName="ccdd_2025_02_03_093258"
 echo "Reading from generation $dbName..."
 echo
 
