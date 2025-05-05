2c28b3c37e312f0220d0d3e4f30461982bb532bc
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2025-05-05 11:37:39 -0400

    updates for May 2025 QA generation

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -7,9 +7,9 @@
 # ARGS (optional)   : qa
 ###############################################################################
 
-ccdd_qa_release_date="20250303"
-ccdd_current_release_date="20250304"
-db_previous_month="ccdd_2025_03_04_112608"
+ccdd_qa_release_date="20250401"
+ccdd_current_release_date="20250401"
+db_previous_month="ccdd_2025_04_01_153826"
 ccdd_current_date=$(date +'%Y%m%d')
 baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 distDir="$baseDir/../dist/$ccdd_current_date"
