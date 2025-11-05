432d241ee3f091812e8ed593d88fb237157b05d1
Corey Yanofsky <corey.yanofsky@hc-sc.gc.ca>
2025-11-05 12:41:51 -0500

    updates for November 2025 QA generation

--- src/sql/setup.sh
+++ src/sql/setup.sh
@@ -7,7 +7,7 @@
 # ARGS (optional)   : qa
 ###############################################################################
 
-ccdd_qa_release_date="20251001"
+ccdd_qa_release_date="20251104"
 ccdd_current_release_date="20251001"
 db_previous_month="ccdd_2025_10_01_155319"
 ccdd_current_date=$(date +'%Y%m%d')
